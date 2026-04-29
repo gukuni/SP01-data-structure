fd59a30ee85bcf0a76d9e15d85bfa2e7b3de1b4d
gukuni <guhkunitaki@gmail.com>
2026-04-29 09:43:51 -0300

    Ajuste de inicio da sessão

--- inicio_de_sessao.c
+++ inicio_de_sessao.c
@@ -1,39 +1,40 @@
 #include <stdio.h>
 #include <time.h>
+#include <stdlib.h>
 
 
 int main() {
 
 
    
-    char cpf_motorista[11];
+    char cpf_motorista[12];
     int carregador;
     char horario[20];
    
    
     printf("     CHARGERID INTELLIGENCE - GOODWE\n");
     printf("------------------------------------------\n");
-    printf("         INICIO DA SESSAO DE RECARGA\n");
+    printf("       INICIO DA SESSAO DE RECARGA\n");
 
 
 
 
    
-    printf("Informe seu CPF: ");
+    printf("Informe seu CPF (apenas numeros): ");
     scanf("%s", cpf_motorista);
 
 
    
     carregador = 0;
+    printf("1 carregador - CCS2 (50kW - 350 kW)\n");
+    printf("2 carregador - AC (7kW - 22kW) \n");
+    printf("3 carregador - CHAdeMO (50kW)\n");
+    printf("Qual carregador voce deseja utilizar? (1 , 2 ou 3)\n");
+    scanf("%d", &carregador);
     while (carregador < 1 || carregador > 3) {
-        printf("Escolha o carregador (1, 2 ou 3): ");
+        printf("  Numero invalido! Digite apenas 1, 2 ou 3.\n\n");
         scanf("%d", &carregador);
-
-
-       
-        if (carregador < 1 || carregador > 3) {
-            printf("  Numero invalido! Digite apenas 1, 2 ou 3.\n\n");
-        }
+        
     }
     time_t agora = time(NULL);
     struct tm *t = localtime(&agora);
@@ -47,6 +48,6 @@ int main() {
    
    
 
-
+    system ("pause");
     return 0;
 }
