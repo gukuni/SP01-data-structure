b22cc8d8360a292c005640f9a568ec8ae375cb35
gukuni <guhkunitaki@gmail.com>
2026-04-29 11:44:57 -0300

    Mudança de nome da file. Atualização da entrada de uso.

--- programa_de_carregador.c
+++ programa_de_carregador.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <time.h>
 #include <stdlib.h>
+#include <string.h>
 
 
 int main() {
@@ -10,6 +11,12 @@ int main() {
     char cpf_motorista[12];
     int carregador;
     char horario[20];
+    time_t inicio, fim;
+    double segundos_usados;
+    double minutos_usados;
+    float preco_por_minuto = 1.50;
+    float total;
+    
    
    
     printf("     CHARGERID INTELLIGENCE - GOODWE\n");
@@ -20,8 +27,16 @@ int main() {
 
 
    
+   while (1) {
     printf("Informe seu CPF (apenas numeros): ");
     scanf("%s", cpf_motorista);
+
+    if (strlen(cpf_motorista) == 11) {
+        break;  
+    } else {
+        printf("  CPF invalido! Digite apenas os 11 numeros.\n\n");
+    }
+}
 
 
    
@@ -36,18 +51,32 @@ int main() {
         scanf("%d", &carregador);
         
     }
+
+    inicio = time(NULL);
     time_t agora = time(NULL);
     struct tm *t = localtime(&agora);
     strftime(horario, 20, "%d/%m/%Y  %H:%M", t);
+
    
    
     printf("  SESSAO INICIADA COM SUCESSO!\n");
-    printf("  Motorista  : %s\n", cpf_motorista);
+    printf("  Usuario  : %s\n", cpf_motorista);
     printf("  Carregador : Box %d\n", carregador);
     printf("  Horario  : %s\n", horario);
-   
-   
 
-    system ("pause");
+    printf("Pressione ENTER para desconectar o carregador...\n");
+    getchar();
+    getchar();
+
+    fim = time(NULL);
+
+    segundos_usados = difftime(fim, inicio);
+    minutos_usados = segundos_usados /60 ;
+    total = minutos_usados * preco_por_minuto;
+    printf("Horario finalizado %s", inicio);
+    printf("Esse e o valor total: %f\n", total);
+    
+
+    
     return 0;
 }
